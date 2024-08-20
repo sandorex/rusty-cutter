6d6ab3ade768e34fc8c316f702d4ed46b47f2625
sandorex <rzhw3h@gmail.com>
2024-08-20 12:18:42 +0000

    Use u64 instead of f64 for the keyframe time, add first test

--- src/cli.rs
+++ src/cli.rs
@@ -1,20 +1,22 @@
 mod util;
 
-use std::process::{Command, ExitCode};
+use std::{process::{Command, ExitCode}, time::Duration};
 use util::CommandOutputExt;
 
 /// Get all keyframes in the file, only works on video files!
 ///
-/// Region can speed things up by limiting the search to that region only (DANGER if too narrow may
-/// exclude the keyframes and screw up things!)
-fn get_keyframes(file: &str, region: Option<(f64, f64)>) -> Result<Vec<f64>, (String, ExitCode)> {
+/// Region can speed things up by limiting the search to that region only
+fn get_keyframes(file: &str, region: Option<(u64, u64)>) -> Result<Vec<u64>, (String, ExitCode)> {
     let mut args: Vec<String> = vec![];
 
     if let Some((start, end)) = region {
+        let start_float = Duration::from_micros(start).as_secs_f64();
+        let end_float = Duration::from_micros(end).as_secs_f64();
+
         // NOTE ffprobe does not care if the start is negative or end is after EOF
         args.extend([
             // read only specified region and add 5s to start and end so keyframes must be found
-            "-read_intervals".into(), format!("{:0.5}%{:0.5}", start - 5.0, end + 5.0),
+            "-read_intervals".into(), format!("{:0.5}%{:0.5}", start_float - 5.0, end_float + 5.0),
         ]);
     }
 
@@ -40,12 +42,20 @@ fn get_keyframes(file: &str, region: Option<(f64, f64)>) -> Result<Vec<f64>, (St
     if cmd.status.success() {
         // parse the time
         let stdout = String::from_utf8_lossy(&cmd.stdout);
-        let mut times: Vec<f64> = vec![];
+        let mut times: Vec<u64> = vec![];
 
         for line in stdout.lines() {
             // im panicking here as it truly should not happen unless something breaks
-            times.push(line.parse::<f64>().expect("Error parsing pts_time from ffprobe"));
+            let time_float = line.parse::<f64>().expect("Error parsing pts_time from ffprobe");
+
+            // im pretty sure u64 can store all the keyframes i can find in a real video..
+            let micros: u64 = Duration::from_secs_f64(time_float).as_micros().try_into().unwrap();
+
+            times.push(micros);
         }
+
+        // the times may not be in correct order sometimes
+        times.sort();
 
         Ok(times)
     } else {
@@ -53,9 +63,32 @@ fn get_keyframes(file: &str, region: Option<(f64, f64)>) -> Result<Vec<f64>, (St
     }
 }
 
-pub fn cut_video(file: &str, start_time: f64, end_time: f64) -> ExitCode {
+fn find_keyframes(keyframes: &Vec<u64>, start_time: u64, end_time: u64) -> Option<(u64, u64)> {
+    // find keyframe that is closes to the start time but not after it
+    let start_keyframe: Option<u64> = keyframes.iter()
+        .filter(|x| start_time >= **x)
+        .cloned()
+        .last();
+
+    // find keyframe that is closes to the end time but not before it
+    let end_keyframe: Option<u64> = keyframes.iter()
+        .filter(|x| end_time <= **x)
+        .cloned()
+        .nth(0);
+
+    match (start_keyframe, end_keyframe) {
+        (Some(start), Some(end)) => Some((start, end)),
+        _ => None,
+    }
+}
+
+pub fn cut_video(file: &str, start_time: u64, end_time: u64) -> ExitCode {
     let keyframes = get_keyframes(file, Some((start_time, end_time)))
         .expect(format!("Unable to get keyframes from {}", file).as_str());
+
+    let time_keyframes = find_keyframes(&keyframes, start_time, end_time);
+
+    dbg!(time_keyframes);
 
     // TODO
     // find first closest frame
@@ -72,10 +105,31 @@ pub fn cut_video(file: &str, start_time: f64, end_time: f64) -> ExitCode {
 }
 
 fn main() -> ExitCode {
-    println!("Hello, world!");
-    let times = get_keyframes("video.mkv", Some((2.0, 10.0)));
-    dbg!(times);
+    cut_video("video.mkv", 2_500_000, 11_000_000);
+    // let times = get_keyframes("video.mkv", Some((2.0, 10.0)));
+    // dbg!(times);
 
     ExitCode::FAILURE
+}
+
+#[cfg(test)]
+mod tests {
+    /// Test if keyframes are properly searched
+    #[test]
+    fn find_keyframes() {
+        use super::find_keyframes;
+
+        let keyframes = vec![0, 2_000_000, 4_000_000, 6_000_000, 8_000_000];
+
+        assert_eq!(
+            find_keyframes(&keyframes, 1_500_000, 2_500_000),
+            Some((0, 4_000_000))
+        );
+
+        assert_eq!(
+            find_keyframes(&keyframes, 2_500_000, 2_500_000),
+            Some((2_000_000, 4_000_000))
+        );
+    }
 }
 
